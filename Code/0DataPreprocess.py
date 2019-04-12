37f1e2003ee950271224d521cc39764dddd435b4
peiyongwang <3014210022@tju.edu.cn>
2019-04-12 23:18:41 +1000

    updates

--- Code/0DataPreprocess.py
+++ Code/0DataPreprocess.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+
 ON_SERVER = True
 if ON_SERVER:
     os.chdir("/home/peiyongw/Research/KT_Project1/COMP90049_Knowledge_Technology_Project_1")
@@ -10,9 +11,9 @@ OUTPUT_DIR = CWD + "/Code_Output/"
 Raw data from LMS
 """
 DATA_DIR = CWD + "/Data/"
-DICT_FILENAME = DATA_DIR+"dict.txt"
-CORRECT_FILENAME = DATA_DIR+"correct.txt"
-MISSPELL_FILENAME = DATA_DIR+"misspell.txt"
+DICT_FILENAME = DATA_DIR + "dict.txt"
+CORRECT_FILENAME = DATA_DIR + "correct.txt"
+MISSPELL_FILENAME = DATA_DIR + "misspell.txt"
 
 dictionary = []
 with open(DICT_FILENAME, 'r') as fp:
@@ -35,9 +36,9 @@ for i in range(len(mis)):
     else:
         entry = [mis[i].rstrip('\n'), correct[i].rstrip('\n'), 'OOV']
     if mis[i].rstrip('\n') == correct[i].rstrip('\n'):
-        entry.append(True) # Means the misspell and the corrected one is actually the same
+        entry.append(True)  # Means the misspell and the corrected one is actually the same
     else:
-        entry.append(False)# Means the misspell and the corrected one is not the same
+        entry.append(False)  # Means the misspell and the corrected one is not the same
     data_entries.append(entry)
 print("Saving misspelling-correct spelling data entries...")
 with open(OUTPUT_DIR + "data_entries.json", 'w') as fp:
@@ -47,7 +48,7 @@ iv = 0
 for c in data_entries:
     if c[2] is 'IV':
         iv = iv + 1
-oov = len(mis)-iv
+oov = len(mis) - iv
 total = len(data_entries)
-print("IV: ", iv, "/", total, '\t', "IV Ratio: ", iv/total)
-print("OOV: ",  oov, "/", total, '\t', "OOV Ratio: ", oov/total)
+print("IV: ", iv, "/", total, '\t', "IV Ratio: ", iv / total)
+print("OOV: ", oov, "/", total, '\t', "OOV Ratio: ", oov / total)
