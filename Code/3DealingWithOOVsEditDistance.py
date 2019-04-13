19de383777fbb3aec48b1a7ffdc91121113a2b21
peiyongwang <3014210022@tju.edu.cn>
2019-04-14 02:46:37 +1000

    updates

--- Code/3DealingWithOOVsEditDistance.py
+++ Code/3DealingWithOOVsEditDistance.py
@@ -5,3 +5,19 @@ import os
 import time
 
 import utilsKTP1 as KTP1
+
+ON_SERVER = True
+if ON_SERVER:
+    os.chdir("/home/peiyongw/Research/KT_Project1/COMP90049_Knowledge_Technology_Project_1")
+CWD = os.getcwd()
+print("Current working directory: ", CWD)
+OUTPUT_DIR = CWD + "/Code_Output/"
+DATA = OUTPUT_DIR + "data_entries.json"
+DICT = OUTPUT_DIR + "dict.json"
+with open(DATA, 'r') as fp:
+    data = json.load(fp)
+with open(DICT, 'r') as fp:
+    dict = json.load(fp)
+
+oov_data = [c for c in data if c[2] == "OOV"]
+print(oov_data)
