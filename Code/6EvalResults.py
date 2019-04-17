87150f21c1a085c55201dd451da38e9a65e1e043
peiyongwang <3014210022@tju.edu.cn>
2019-04-18 00:15:51 +1000

    updates

--- Code/6EvalResults.py
+++ Code/6EvalResults.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+os.chdir("/home/peiyongw/Research/KT_Project1/COMP90049_Knowledge_Technology_Project_1/")
 CWD = os.getcwd()
 print(CWD)
 OUTPUT_DIR = CWD + "/Code_Output/"
