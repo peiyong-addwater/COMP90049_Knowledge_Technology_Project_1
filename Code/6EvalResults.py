0d0f56bf185b8eca92b3db3171c35904b276b123
peiyongwang <3014210022@tju.edu.cn>
2019-04-18 17:34:16 +1000

    updates

--- Code/6EvalResults.py
+++ Code/6EvalResults.py
@@ -5,7 +5,7 @@ os.chdir("/home/peiyongw/Research/KT_Project1/COMP90049_Knowledge_Technology_Pro
 CWD = os.getcwd()
 print(CWD)
 OUTPUT_DIR = CWD + "/Code_Output/"
-a = "distance_matching_result.json"
+a = "soundex_matching_result.json"
 with open(OUTPUT_DIR + a, 'r') as f:
     data = json.load(f)
 print(len(data))
